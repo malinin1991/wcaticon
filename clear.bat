05bd0fcf339f4369d45524aa0de99872ce8bc949
malinin1991 <malininevgeniy@gmail.com>
2014-08-24 14:28:34 +0700

    1.0.4

--- clear.bat
+++ clear.bat
@@ -1,3 +1,4 @@
 @echo off
 del /q res_mods\xvm\res\clanicons\RU\nick\programist434.png
 del /q res_mods\xvm\res\clanicons\RU\nick\Morozochka.png
+del /q res_mods\xvm\res\clanicons\RU\nick\gipof1s.png
